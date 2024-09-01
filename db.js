603f2f87a6e0935c46a44337aad5860f71e26514
shadab1969 <shadabanwar2050@gmail.com>
2024-09-02 00:37:12 +0530

    first version of my hotel

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+//Define the MongoDB connection URL
+const mongoURL='mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true  
+})
+
+//Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db=mongoose.connection;
+
+//Define events listeners for database connection
+db.on('connected',()=>{
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error',(err)=>{
+    console.error('MongoDB error:',err);
+});
+
+db.on('disconnected',()=>{
+    console.log('MongoDB disconnected');
+});
+
+//Export the database connection
+module.exports=db;
