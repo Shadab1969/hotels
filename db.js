107b5200a437a0ba03fba1c71e7be0983ddb572d
shadab1969 <shadabanwar2050@gmail.com>
2024-11-03 19:07:03 +0530

    authentication and cors update

--- db.js
+++ db.js
@@ -3,18 +3,14 @@ require('dotenv').config();
 //Define the MongoDB connection URL
 //const mongoURL='mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name
 
-//const mongoURL=process.env.MongoDB_URL_LOCAL             // offline
-const mongoURL=process.env.DB_URL;                      //online
+const mongoURL=process.env.MongoDB_URL_LOCAL                 // offline
+//const mongoURL=process.env.DB_URL;                        //online
 
 
 
 // Set up MongoDB connection
-
-//mongoose.connect(mongoURL,{
-  //useNewUrlParser:true,
-  //useUnifiedTopology:true
-//})
 mongoose.connect(mongoURL)
+
 //Get the default connection
 // Mongoose maintains a default connection object representing the MongoDB connection.
 const db=mongoose.connection;
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express=require('express')
 const app=express();
 const db=require('./db');
-//require('dotenv').config();
+require('dotenv').config();
+const passport= require('./auth.js');
+const cors= require('cors')
+
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());//req.body
 const PORT=process.env.PORT || 3000;
 
+//Middleware for cors
+app.use(cors());
 
-app.get('/',function(req,res){
+//Middleware Function
+const logrequest=(req,res,next)=>{
+  console.log(`${new Date().toLocaleString()} Request Made to: ${req.originalUrl}`);
+  next();  //Move on to the next phase
+}
+app.use(logrequest);
+
+
+ app.use(passport.initialize());
+//const localAuthMiddleware = passport.authenticate('local', {session: false});
+
+app.get('/' ,function(req,res){
   res.send('Welcome to my hotel...How i can help you ?, we have list of menus')
 })
 
