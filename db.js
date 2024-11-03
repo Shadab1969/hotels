16afbb69ed2124ee7a7631d6380df2988677ac1c
shadab1969 <shadabanwar2050@gmail.com>
2024-11-03 19:25:27 +0530

    mongo online choosen

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 //Define the MongoDB connection URL
 //const mongoURL='mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name
 
-const mongoURL=process.env.MongoDB_URL_LOCAL                 // offline
-//const mongoURL=process.env.DB_URL;                        //online
+// const mongoURL=process.env.MongoDB_URL_LOCAL                 // offline
+const mongoURL=process.env.DB_URL;                        //online
 
 
 
